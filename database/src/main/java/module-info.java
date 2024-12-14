6e49327354c2ce7f7c91e89d90811b8253a5a4d1
Aiidanaqq <150505764+Aiidanaqq@users.noreply.github.com>
2024-12-15 02:32:45 +0600

    Delete database directory

--- database/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.movie {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens org.example.movie to javafx.fxml;
-    exports org.example.movie;
-}
